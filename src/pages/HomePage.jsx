34507244082b979b4ab3aeaaa9ee9b3f0848fe8f
dennie <denitsa.sureva@icloud.com>
2026-04-10 20:13:53 +0200

    updated home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+
 import DiscountSection from "../components/DiscountSection";
 import FeatureSection from "../components/features/FeatureSection";
 import Footer from "../components/Footer";
@@ -7,25 +7,14 @@ import Hero from "../components/Hero";
 import ProductSection from "../components/products/ProductSection";
 
 export default function HomePage() {
-    const [products, setProducts] = useState([]);
-    
-        const getProducts = async () => {
-            const res = await fetch('https://dummyjson.com/products/category/beauty?limit=3');
-            const data = await res.json();
-           setProducts(data.products)
-        }
-    
-        console.log(products)
-        useEffect (() => {
-            getProducts()
-        }, [])
+   
     
     return (
         <>
             <Header />
             <Hero />
             <FeatureSection />
-            <ProductSection title={"Best Selling Skincare"} products={products}/>
+            <ProductSection title={"Best Selling Skincare"} />
             <DiscountSection />
             <Footer />
         </>
