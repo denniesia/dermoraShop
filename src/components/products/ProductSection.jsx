60a5244ab5939c73d4d6aff820ee90e999590d4e
dennie <denitsa.sureva@icloud.com>
2026-04-10 20:22:09 +0200

    adding loading spinner

--- src/components/products/ProductSection.jsx
+++ src/components/products/ProductSection.jsx
@@ -1,24 +1,33 @@
+import Loading from "../Loading";
 import ProductItem from "./ProductItem";
 import { useEffect, useState } from "react";
 
 export default function ProductSection({ title = "" }) {
     const [products, setProducts] = useState([]);
-
-    const getProducts = async () => {
-        const res = await fetch('https://dummyjson.com/products/category/beauty?limit=3');
-        const data = await res.json();
-        setProducts(data.products)
-    }
-
-    console.log(products)
+    const [loading, setLoading] = useState(false);
+    
     useEffect(() => {
+        async function getProducts() {
+            try {
+                const res = await fetch('https://dummyjson.com/products/category/beauty');
+                const data = await res.json();
+                setProducts(data.products)
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching products", error);
+                setLoading(false)
+            }
+        }
         getProducts()
     }, [])
+
+
     return (
         <section id="products" className="py-16 max-w-7xl mx-auto px-8 text-pink-500">
             <h2 className="text-4xl font-bold text-center mb-12">
                 {title}
             </h2>
+            {loading && <Loading/>}
             <div className="grid md:grid-cols-3 gap-8">
                 {products.map((product) => (
                     <ProductItem
--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -6,24 +6,7 @@ import ProductSection from "../components/products/ProductSection";
 import TitleSection from "../components/TitleSection";
 
 export default function ShopPage() {
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(true);
 
-    useEffect (() => {
-        async function getProducts() {
-            try {
-                const res = await fetch('https://dummyjson.com/products/category/beauty');
-                const data = await res.json();
-                setProducts(data.products)
-                setLoading(false);
-            } catch(error) {
-                console.error("Error fetching products", error);
-                setLoading(false)
-            }
-            
-        }
-        getProducts()
-    }, [])
 
     return (
         <>
@@ -31,7 +14,7 @@ export default function ShopPage() {
             <TitleSection title={"Shop Skincare"} subtitle={"Explore products by category"}/>
             <CategoryFilter/>
 
-            <ProductSection products={products}/>
+            <ProductSection/>
             <Footer />
         </>
     );
