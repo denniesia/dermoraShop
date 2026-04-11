3265890c9ca6f655a16a6779898dc3781f7fd6e0
dennie <denitsa.sureva@icloud.com>
2026-04-11 18:28:40 +0200

    add cart icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
+import { FiShoppingCart } from "react-icons/fi";
+
 export default function Header() {
     return (
         <header className="sticky top-0 bg-white/80 backdrop-blur z-50 flex items-center justify-between px-8 py-4 shadow-sm">
             <a href="/"><h1 className="text-3xl font-bold tracking-tight text-pink-500 ml-10">Dermora Shop</h1></a>
-            <nav className="space-x-8 text-m font-medium">
+            <nav className="flex items-center gap-8 text-md font-medium">
                 <a href="/" className="hover:text-pink-500">
                     Home
                 </a>
-                <a href="/shop" className="hover:text-black text-pink-500">
+
+                <a href="/shop" className="text-pink-500 hover:text-black">
                     Shop
                 </a>
-                <a href="#about" className="hover:text-pink-500">
-                    About
-                </a>
-                <a href="#contact" className="hover:text-pink-500">
-                    Contact
+
+                <a href="#" className="flex items-center gap-2 hover:text-pink-500">
+                    <FiShoppingCart className="w-6 h-6 " />
                 </a>
             </nav>
         </header>
