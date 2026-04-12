6d57551848d35b2369f36e944cc8639d7d4d971f
dennie <denitsa.sureva@icloud.com>
2026-04-12 19:03:24 +0200

    updating text on hero comp + buy link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,14 +3,15 @@ export default function Hero() {
         <section className="grid md:grid-cols-2 items-center px-8 py-16 gap-10 max-w-7xl mx-auto">
             <div>
                 <h2 className="text-5xl font-extrabold leading-tight mb-6">
-                   Shop Everything You Need in One Place
+                   Healthy Skin Starts Here
                 </h2>
                 <p className="text-lg text-gray-600 mb-8">
-                    Explore a wide range of quality products, from everyday essentials to the latest trends—all carefully selected to give you the best shopping experience.
+                    Clinically tested, naturally derived skincare designed to hydrate,
+                        repair, and enhance your glow.
                 </p>
-                <button className="bg-pink-500 text-white px-8 py-4 rounded-2xl shadow-lg hover:bg-pink-600 transition">
+                <a href="/shop" className="bg-pink-500 text-white px-8 py-4 rounded-2xl shadow-lg hover:bg-pink-600 transition">
                     Shop Now
-                </button>
+                </a>
             </div>
             <img
                 loading="lazy"
