267690192eee38ad4e77fe5e273124b2f79c0e50
dennie <denitsa.sureva@icloud.com>
2026-04-19 17:57:42 +0200

    sonner installation + config

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
 import ShopPage from "./pages/ShopPage";
-import  CartProvider  from "./contexts/cart/CartProvider";
+import CartProvider from "./contexts/cart/CartProvider";
 import CartPage from "./pages/CartPage";
+import { Toaster } from "sonner";
 
 
 function App() {
     return (
+        <>
+            <Toaster position="bottom-right" toastOptions={{
+                style: {
+                     background: "white",
+                    color: "#FD2666",
+                    border: "1px solid #FD2666",
+                },
+            }} />
 
-        <CartProvider>
-            <Routes>
-                <Route index element={<HomePage />} />
-                <Route path="/shop" element={<ShopPage />} />
-                <Route path="/cart" element={<CartPage />} />
-            </Routes>
-        </CartProvider>
-
+            <CartProvider>
+                <Routes>
+                    <Route index element={<HomePage />} />
+                    <Route path="/shop" element={<ShopPage />} />
+                    <Route path="/cart" element={<CartPage />} />
+                </Routes>
+            </CartProvider>
+        </>
     );
 }
 
